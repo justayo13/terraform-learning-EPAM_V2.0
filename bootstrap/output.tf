a64a9ce06733fdbea8159d8c25da2aa6389447ee
justayo13 <javier.hernandezg13@gmail.com>
2025-07-01 22:45:19 -0600

    Fourth commit: Fixing backend setup for Terraform Learning EPAM

--- bootstrap/output.tf
+++
@@ -1,5 +0,0 @@
-# Output the name of the bucket to be used in the backend configurations
-output "gcs_bucket_name" {
-  description = "The name of the GCS bucket for the Terraform backend."
-  value       = google_storage_bucket.backend.name
-}
